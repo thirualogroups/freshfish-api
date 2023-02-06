b5567dd77b3f6be5db8196394e4d64ea9f7935ec
Thiru <thirualogroups@gmail.com>
2023-02-06 13:25:39 +0530

    cart

--- routes/newproduct_detail.routes.js
+++ routes/newproduct_detail.routes.js
@@ -221,7 +221,7 @@ router.post('/mobile/cart/delete', async function (req, res){
     });
 });
 
-router.post('/mobile/cart/getlist1', async function (req, res){
+router.post('/mobile/cart/getlist', async function (req, res){
 
   const cart_details = await cart_detailsModel.find({user_id: new mongoose.Types.ObjectId(req.body.user_id),delete_status:false}).populate('product_details_id');
   
@@ -271,40 +271,41 @@ router.post('/mobile/cart/getlist1', async function (req, res){
 
       });
 
-router.post('/mobile/cart/getlist', async function (req, res){
-  const cart_details = await cart_detailsModel.find({user_id: new mongoose.Types.ObjectId(req.body.user_id),delete_status:false}).populate('product_details_id');
-  console.log("cart_details",cart_details);
-  if(cart_details.length == 0){
-    res.json({ Status: "Success", Message: "Your Card Details is Empty", Data: [], Code: 200 });
-  }
-  var cart_final_value = [];
-  for(let a = 0; a < cart_details.length ; a++){ 
-  cart_details[a].product_details_id.soldout  = false;
-  cart_details[a].product_details_id.related  = "";
-  let stock_params = {fish_combo_id: new mongoose.Types.ObjectId(cart_details[a].product_details_id.fish_combo_id), status: true, delete_status: false, soldout: false, store:req.body.store_id };
-  let stock = await stockModel.findOne(stock_params);
-  console.log(stock);
+// router.post('/mobile/cart/getlist', async function (req, res){
+//   const cart_details = await cart_detailsModel.find({user_id: new mongoose.Types.ObjectId(req.body.user_id),delete_status:false}).populate('product_details_id');
+//   console.log("cart_details",cart_details);
+//   if(cart_details.length == 0){
+//     res.json({ Status: "Success", Message: "Your Card Details is Empty", Data: [], Code: 200 });
+//   }
+//   var cart_final_value = [];
+//   for(let a = 0; a < cart_details.length ; a++){ 
+//   cart_details[a].product_details_id.soldout  = false;
+//   cart_details[a].product_details_id.related  = "";
+//   let stock_params = {fish_combo_id: new mongoose.Types.ObjectId(cart_details[a].product_details_id.fish_combo_id), status: true, delete_status: false, soldout: false, store:req.body.store_id };
+//   let stock = await stockModel.findOne(stock_params);
+//   console.log(stock);
 
-   if(stock == null){
-    cart_details[a].product_details_id.soldout  = true;
-    cart_details[a].product_details_id.related  = "Sold Out";
-  }else if(stock.soldout == true){
-    cart_details[a].product_details_id.soldout  = true;
-    cart_details[a].product_details_id.related  = "Sold Out";
-  }else if(stock.gross_weight == 0){
-    cart_details[a].product_details_id.soldout  = true;
-    cart_details[a].product_details_id.related  = "NO Available";
-  }else if(stock.gross_weight <= +cart_details[a].gross_weight){
-    cart_details[a].product_details_id.soldout  = true;
-    cart_details[a].product_details_id.related  = "Stock is less";
-  }
-  //console.log("Stock Value Status",cart_details[a].product_details_id.soldout);
-  cart_final_value.push(cart_details[a]);
-  if(a == cart_details.length - 1){
-    res.json({ Status: "Success", Message: "Your Card Details", Data: cart_final_value, Code: 200 });
-  }
- }
-});
+//    if(stock == null){
+//     cart_details[a].product_details_id.soldout  = true;
+//     cart_details[a].product_details_id.related  = "Sold Out";
+//   }else if(stock.soldout == true){
+//     cart_details[a].product_details_id.soldout  = true;
+//     cart_details[a].product_details_id.related  = "Sold Out";
+//   }else if(stock.gross_weight == 0){
+//     cart_details[a].product_details_id.soldout  = true;
+//     cart_details[a].product_details_id.related  = "NO Available";
+//   }else if(stock.gross_weight <= +cart_details[a].gross_weight){
+//     cart_details[a].product_details_id.soldout  = true;
+//     cart_details[a].product_details_id.related  = "Stock is less";
+//   }
+//   //console.log("Stock Value Status",cart_details[a].product_details_id.soldout);
+//   cart_final_value.push(cart_details[a]);
+//   if(a == cart_details.length - 1){
+//     res.json({ Status: "Success", Message: "Your Card Details", Data: cart_final_value, Code: 200 });
+//   }
+//  }
+// });
+
 router.post('/mobile/cart/removeall', async function (req, res){
 var remove_user_cart={user_id:req.body.user_id};
   await cart_detailsModel.remove(remove_user_cart, function (err, user) {
