8c2728e7625c47b89b42f6b0c0811725cb28e790
Thiru <thirualogroups@gmail.com>
2023-02-06 18:46:20 +0530

    hii

--- routes/order_details.routes.js
+++ routes/order_details.routes.js
@@ -942,12 +942,13 @@ router.post("/payment-link", async (req, res) => {
         "customerEmail":user.user_email,
         "customerMobile": user.user_phone,
       },
+      "invoicePhoneNo":user.user_phone,
       //  "invoiceDetails":{
       //   "productName":order_details.,
       //   "noOfUnits":,
       //   "perUnitAmount":orderdetails.order_details[a].price
       // },
-      //"statusCallbackUrl":"https://weknowfreshfish.com/api/order_details/callbackurl",
+      "statusCallbackUrl":"https://securegw.paytm.in/theia/paytmCallback?ORDER_ID="+req.body.orderid,
       "redirectionUrlSuccess":"http://ec2-44-208-166-141.compute-1.amazonaws.com/#/billing-details?success="+req.body.orderid,
       "redirectionUrlFailure":"http://ec2-44-208-166-141.compute-1.amazonaws.com/#/billing-details?failed="+req.body.orderid
   };
