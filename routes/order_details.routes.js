f3f9ed6ca67449771a3d3059d1a2160078afbee6
Thiru <thirualogroups@gmail.com>
2023-02-07 18:32:08 +0530

    gggg

--- routes/order_details.routes.js
+++ routes/order_details.routes.js
@@ -406,8 +406,8 @@ router.post('/getlist/order_id', async function (req, res) {
    
  });
 
- router.post('/getlist/vendor_id', async function (req, res) {
-  if(req.body.user_type==3){
+ router.get('/getlist/vendor_id', async function (req, res) {
+  if(user_type == 3){
 
  
   var a = {
@@ -415,7 +415,7 @@ router.post('/getlist/order_id', async function (req, res) {
   }
 
 
-  let your_order= await order_detailsModel.find({vendor_id:req.body.vendor_id},a).populate([{ path: "user_id", select: ["first_name", "middle_name", "last_name", "user_email", "user_phone", "user_address"] },{path: "store", select: ["name","phoneno","email","location","type","address","code"] },
+  let your_order= await order_detailsModel.find({vendor_id:req.query.vendor_id},a).populate([{ path: "user_id", select: ["first_name", "middle_name", "last_name", "user_email", "user_phone", "user_address"] },{path: "store", select: ["name","phoneno","email","location","type","address","code"] },
   { path: "vendor_id", select: ["business_name", "code", "store"], populate: [{ path: "store", select: ["name", "phoneno", "email"] }] },
   { path: "order_details.product_id", select: ["fish_combo_id", "unit", "price_type", "min_net_weight", "max_net_weight", "gross_weight", "cost", "discount_amount", "cat_id", "thumbnail_image", "product_img"], populate: [{ path: "fish_combo_id", select: ["product_name"] }, { path: "cat_id", select: ["product_cate"] }] }, { path: "shippingid" }]);
  if(your_order!==null){
