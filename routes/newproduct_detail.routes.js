643ed9feed9a74e9cae9060551dd2c607b3e2292
Thiru <thirualogroups@gmail.com>
2023-02-28 11:44:05 +0530

    gj

--- routes/newproduct_detail.routes.js
+++ routes/newproduct_detail.routes.js
@@ -511,7 +511,7 @@ router.post('/mobile/slot-alert',async function (req, res) {
     let pincode_params={user_id:default_user[i]};
     let default_pincodes= await shipping_addressModel.findOne(pincode_params);
     let vendor = await product_vendorModel.findOne({ pincodes: { $elemMatch: { $eq: default_pincodes.pincode } }, status: true, delete_status: false });
-    vendorlist.push(vendor.delivery_slots);
+    vendorlist.push(vendor[i].delivery_slots);
 
     if(vendorlist[i] != null){
     let users=await userdetailsModel.findOne({_id:default_user[i]});
