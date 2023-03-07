5569b9b8a276d0e42cd0bd67401ab2c5b3d862e4
Thiru <thirualogroups@gmail.com>
2023-03-07 17:21:02 +0530

    checkout api

--- routes/newproduct_detail.routes.js
+++ routes/newproduct_detail.routes.js
@@ -350,13 +350,12 @@ var remove_user_cart={user_id:req.body.user_id};
 
 
 router.post('/check_checkout_stock',async function (req, res) {
- console.log(req.body)
   var final_value = [];
   for(let a = 0; a < req.body.check_value.length ; a++){ 
   var temp_value = req.body.check_value[a];
   let stock_params = {fish_combo_id: new mongoose.Types.ObjectId(temp_value.fish_combo_id), status: true, delete_status: false, soldout: false, store:temp_value.store };
   let stock = await stockModel.findOne(stock_params);
-  console.log("stock",stock);
+
   var soldout  = false;
   var related  = "";
   if(stock == null){
@@ -368,11 +367,10 @@ router.post('/check_checkout_stock',async function (req, res) {
   }else if(stock.gross_weight == 0){
      soldout  = true;
      related  = "NO Available";
-  }else if(stock.gross_weight < +temp_value.gross_weight){
+  }else if(stock.gross_weight < +temp_value.gross_wt){
      soldout  = true;
      related  = "Stock is less";
   }
-  console.log(soldout,related);
   req.body.check_value[a].soldout = soldout;
   req.body.check_value[a].related = related;
 
